8f337ba0de0194896e83383c8581113393aafec6
shivasaireddy <shivasaireddyp@gmail.com>
2025-10-07 21:00:05 +0530

    changed the sentence transformer model

--- backend/utils/search.py
+++ backend/utils/search.py
@@ -1,7 +1,7 @@
 from sentence_transformers import SentenceTransformer
 import faiss
 class SemanticSearch:
-    def __init__(self, model_name="all-MiniLM-L6-v2"):
+    def __init__(self, model_name="paraphrase-albert-small-v2"):
         self.model = SentenceTransformer(model_name)
         self.texts = []
         self.meta = []
